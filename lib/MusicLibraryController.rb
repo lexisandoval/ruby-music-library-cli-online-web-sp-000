65a99332c0811712e0fcfdd0fa5063883e51f535
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-02-09 22:57:55 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -73,8 +73,7 @@ class MusicLibraryController
     genre = Genre.find_by_name(input)
     genre.songs.sort{|a,b| a.name <=> b.name}.each_with_index do |song, i|
       puts "#{i+1}. #{song.artist.name} - #{song.name}"
-    end
-    
+    end  
   end
 
   def play_song
