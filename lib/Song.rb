054c6c0dbb5393a30e2d19ea9cc44107941f1ca7
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-02-09 22:28:34 +0000

    Automatically backed up by Learn

---
+++ lib/Song.rb
@@ -0,0 +1,64 @@
+class Song
+  extend Concerns::Findable
+  attr_accessor :name, :artist, :genre
+  @@all = []
+
+  def initialize(name, artist=nil, genre=nil)
+    @name = name
+    self.artist=(artist) if artist !=nil
+    self.genre=(genre) if genre !=nil
+  end
+
+  def self.all
+    @@all
+  end
+
+  def genre
+    @genre
+  end
+
+  def self.new_from_filename(filename)
+    array = filename.split(" - ")
+
+    artist_name = array[0]
+    song_name = array[1]
+    genre_name = array[2].split(".mp3").join
+
+    genre = Genre.find_or_create_by_name(genre_name)
+    artist = Artist.find_or_create_by_name(artist_name)
+
+    self.new(song_name, artist, genre)
+  end
+
+  def self.create_from_filename(filename)
+    self.new_from_filename(filename).save
+  end
+
+  def genre=(genre)
+    @genre = genre
+    genre.songs << self unless genre.songs.include?(self)
+  end
+
+  def artist
+    @artist
+  end
+
+  def artist=(artist)
+    @artist = artist
+    artist.add_song(self)
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def self.create(song)
+    song = self.new(song)
+    song.save
+    song
+  end
+end
