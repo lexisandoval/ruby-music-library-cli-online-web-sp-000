f0eb283c864a520efe8349b4c4ef45956a7d2fae
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-02-09 22:41:13 +0000

    Automatically backed up by Learn

--- lib/Song.rb
+++ lib/Song.rb
@@ -30,15 +30,17 @@ class Song
     song
   end
 
+  def artist=(artist)
+    @artist = artist
+    artist.add_song(self)
+  end
+
   def genre=(genre)
     @genre = genre
     genre.songs << self unless genre.songs.include?(self)
   end
 
-  def artist=(artist)
-    @artist = artist
-    artist.add_song(self)
-  end
+
 
   def self.new_from_filename(filename)
     array = filename.split(" - ")
