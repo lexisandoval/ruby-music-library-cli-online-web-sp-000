19c67cb509f66565286af6a1d01d721406e1c389
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-02-09 22:38:57 +0000

    Automatically backed up by Learn

--- lib/Song.rb
+++ lib/Song.rb
@@ -3,13 +3,13 @@ class Song
   extend Concerns::Findable
 
   attr_accessor :name, :artist, :genre
-  
+
   @@all = []
 
   def initialize(name, artist=nil, genre=nil)
     @name = name
-    self.artist=(artist) if artist !=nil
-    self.genre=(genre) if genre !=nil
+    self.artist=(artist) if artist != nil
+    self.genre=(genre) if genre != nil
   end
 
   def self.all
